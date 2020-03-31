a0c29d66b316b3b4a63ebb87cf0df0e6cb9fb5d3
tyson <tyelf22@hotmail.com>
2020-03-30 23:37:35 -0600

    added players to store, ability to add players

--- src/components/AssignRoles.vue
+++ src/components/AssignRoles.vue
@@ -1,8 +1,5 @@
 <template>
   <v-app id="inspire">
-
-   
-    
     <!-- <v-navigation-drawer
       class="top"
       v-model="drawer"
@@ -27,97 +24,68 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer> -->
-
- 
-    
+    </v-navigation-drawer>-->
 
     <v-content>
-     
-      <v-container
-        class="fill-height"
-        fluid
-      >
-   
-        <v-row
-          align="center"
-          justify="center"
-        >
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
           <v-col class="shrink">
-            
-                   <div class="text-center">
-  <h1>WereWolf</h1>
-  <br>
-  <br>
-  
- <p>ROLES</p>
-  <br>
+            <div class="text-center">
+              <h1>WereWolf</h1>
+              <br />
+              <br />
 
-  <br>
-  <div>
-                    
-  <v-text-field
-    label="Solo"
-    placeholder="Name"
-    solo
-  ></v-text-field>
-    
-</div>
-<div>
-<router-link to="/SecondPage" class="none">
-<v-btn x-large color="#AA5F2C" dark id="start">
-add</v-btn></router-link> 
-</div>
-<br>
-<br>
- 
- </div>    
-       
-</v-col>
-         
-</v-row>
-<router-link to="/NightPhase" class="next">Next Step</router-link> 
-</v-container>
-              
+              <p>ROLES</p>
+              <br />
 
+              <br />
+              <div>
+                <v-text-field label="Solo" placeholder="Name" solo></v-text-field>
+              </div>
+              <div>
+                <router-link to="/SecondPage" class="none">
+                  <v-btn x-large color="#AA5F2C" dark id="start">add</v-btn>
+                </router-link>
+              </div>
+              <br />
+              <br />
+            </div>
+          </v-col>
+        </v-row>
+        <router-link to="/NightPhase" class="next">Next Step</router-link>
+      </v-container>
     </v-content>
-
-   
-</v-app>
+  </v-app>
 </template>
+
+
 <style scoped>
 .next {
-padding-left: 190px ;
-text-decoration: none;
-color: white;
-font-size: 20px;
- letter-spacing: .4px;
-
+  padding-left: 190px;
+  text-decoration: none;
+  color: white;
+  font-size: 20px;
+  letter-spacing: 0.4px;
 }
-
 
 h1 {
   letter-spacing: 2px;
-
 }
 p {
   color: white;
   letter-spacing: 1px;
-  font-size:20px;
+  font-size: 20px;
 }
 #start {
-    border-radius: 10px;
+  border-radius: 10px;
 }
-
-
 </style>
 <script>
-  export default {
-    name: 'AssignRoles',
+export default {
+  name: "AssignRoles",
 
-
-    data: () => ({
-      title: 'This is the second page'
-    }),
-  }
+  data: () => ({
+    title: "This is the second page"
+  })
+};
 </script>
